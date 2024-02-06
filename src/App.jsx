ce4a03a037d97c72b8391aaef80e88d3296f0643
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-02-06 03:08:40 +0100

    Routes for new pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Team from "./pages/Team";
+import Fakeye from "./pages/Fakeye";
+import Contact from "./pages/Contact";
+import Catalogue from "./pages/Catalogue";
 
 function App() {
 	return (
@@ -9,7 +12,10 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
-          <Route path="/team" element={<Team/>} />
+					<Route path="/team" element={<Team />} />
+					<Route path="/team/abolarin-fakeye" element={<Fakeye />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/real-estate" element={<Catalogue />} />
 				</Routes>
 			</BrowserRouter>
 		</>
