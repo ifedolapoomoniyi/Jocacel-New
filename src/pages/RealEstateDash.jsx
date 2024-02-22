5fb13ab2a7d3c44b324c16df6c573d4bb050246e
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-02-22 05:17:05 +0100

    Real estate dashboard

---
+++ src/pages/RealEstateDash.jsx
@@ -0,0 +1,23 @@
+import Sidebar from "../components/Sidebar";
+import TopBar from "../components/TopBar";
+
+export const RealEstateDash = () => {
+	return (
+		<div>
+			<Sidebar />
+
+			{/* Manage realestate */}
+			<div>
+				<TopBar />
+
+				<input type="file" name="" id="" />
+
+				<div>
+					<h2>Real Estate Properties</h2>
+
+					<div></div>
+				</div>
+			</div>
+		</div>
+	);
+};
