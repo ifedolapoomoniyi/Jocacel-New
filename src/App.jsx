88e0d83b5e2f6ab78f207f42503c8778aa02c131
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-02-28 03:58:48 +0100

    Routing for new pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Property from "./pages/Property";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import TeamUpload from "./pages/TeamUpload";
+import Projects from "./pages/Projects";
+import GalleryDash from "./pages/GalleryDash";
+import { RealEstateDash } from "./pages/RealEstateDash";
 
 function App() {
 	return (
@@ -20,10 +23,23 @@ function App() {
 					<Route path="/team/abolarin-fakeye" element={<Fakeye />} />
 					<Route path="/contact" element={<Contact />} />
 					<Route path="/real-estate" element={<Catalogue />} />
-					<Route path="/real-estate/:propertyname" element={<Property/>} />
-					<Route path="/login" element={<Login/>} />
-					<Route path="/dashboard" element={<Dashboard/>} />
-					<Route path="/dashboard/team" element={<TeamUpload/>} />
+					<Route
+						path="/real-estate/:propertyname"
+						element={<Property />}
+					/>
+					<Route path="/login" element={<Login />} />
+					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/dashboard/team" element={<TeamUpload />} />
+					<Route path="/team/:id" element={<Fakeye />} />
+					<Route path="/gallery" element={<Projects />} />
+					<Route
+						path="/dashboard/gallery"
+						element={<GalleryDash />}
+					/>
+					<Route
+						path="/dashboard/real-estate"
+						element={<RealEstateDash />}
+					/>
 				</Routes>
 			</BrowserRouter>
 		</>
