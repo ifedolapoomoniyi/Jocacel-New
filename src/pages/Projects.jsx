5813625ac5bbdd1f2955d6a91002b6b281098830
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-03-25 13:05:56 +0100

    Responsiveness plus contact card

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,6 +7,9 @@ import Footer from "../components/Footer";
 import { useState } from "react";
 import { BASE_URL } from "../utils";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import HouseImg from "../assets/HouseImg.png";
+import { GoArrowRight } from "react-icons/go";
 
 const Projects = () => {
 	const [teamData, setTeamData] = useState([]);
@@ -25,7 +28,7 @@ const Projects = () => {
 			<Navbar />
 
 			{/* Hero */}
-			<div className="gallery-hero text-white h-screen flex flex-col items-center justify-center p-4">
+			<div className="flex flex-col items-center justify-center h-screen p-4 text-white gallery-hero">
 				<h1 className="p-4 text-3xl font-bold text-center">
 					Our Projects Gallery
 				</h1>
@@ -39,9 +42,16 @@ const Projects = () => {
 
 			{/* Past projects */}
 
-			<h2 className="p-10 font-bold text-3xl">Past Projects</h2>
+			<h2 className="p-10 text-3xl font-bold">Past Projects</h2>
 
-			<div className="flex flex-row gap-5 p-5 pb-10 flex-wrap items-center">
+			<div className="flex flex-row flex-wrap items-center justify-center gap-5 p-5 pb-10 md:justify-normal">
+				<Gallery
+					details={
+						"Construction of 5 Bedroom Duplex Private Development at Sango Ota, Ogun State"
+					}
+					image={PropertyImg1}
+				/>
+
 				{teamData.map((project) => (
 					<Gallery
 						key={project._id}
@@ -49,7 +59,7 @@ const Projects = () => {
 						image={RealEstateImg}
 					/>
 				))}
-				
+
 				<Gallery
 					details={
 						"Construction of 5 Bedroom Duplex Private Development at Sango Ota, Ogun State"
@@ -59,7 +69,29 @@ const Projects = () => {
 			</div>
 
 			{/* Contact us */}
-			<div className="p-5"></div>
+			<div className="p-5 w-[75%] lg:w-[65%] green-black-gradient m-auto rounded-xl text-white flex items-center flex-col relative">
+				<div className="max-w-[400px] m-auto flex flex-col items-center z-10">
+					<h3 className="py-3 text-xl font-bold md:text-2xl">Like Our Work?</h3>
+					<div>
+						Quality is never an accident, it is always the result of
+						intelligent effort. Here are some of our recent building
+						projects
+					</div>
+					<Link
+						to="/contact"
+						className="flex flex-row p-1 px-2 my-3 mb-5 bg-white rounded-md text-primary hover:animate-pulse"
+					>
+						Contact Us <GoArrowRight />
+					</Link>
+				</div>
+				<div>
+					<img
+						src={HouseImg}
+						alt=""
+						className="absolute h-52 bottom-2 right-2"
+					/>
+				</div>
+			</div>
 
 			<Footer />
 		</div>
