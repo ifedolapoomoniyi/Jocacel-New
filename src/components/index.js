dd32e4caf7665ee04e10de3df5a5b788ac43bbe1
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-04-07 17:54:19 +0100

    Resolve folder exports into index file to help resolve bug

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import RealEstate from "./RealEstate";
+import Gallery from "./Gallery";
+import AdminProject from "./AdminProject";
+import AdminRealEstate from "./AdminRealEstate";
+import BtnPrimary from "./BtnPrimary";
+import Footer from "./Footer";
+import ImgSlider from "./ImgSlider";
+import Sidebar from "./Sidebar";
+import TeamCard from "./TeamCard";
+import TopBar from "./TopBar";
+
+export {
+	Navbar,
+	RealEstate,
+	Gallery,
+	AdminProject,
+	AdminRealEstate,
+	BtnPrimary,
+	Footer,
+	ImgSlider,
+	Sidebar,
+	TeamCard,
+  TopBar
+};
