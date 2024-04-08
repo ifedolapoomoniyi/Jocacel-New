a6e253c0ffd1dba682038933dcb130e1b6f4af40
TSOlami <tijani.saheed@yahoo.com>
2024-04-08 10:57:44 +0100

    Refactor imports and add convertToBase64 utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api"
+import { convertToBase64 } from "./convertToBase64";
+
+export const BASE_URL = "http://localhost:5000/api";
+
+export { convertToBase64 };
