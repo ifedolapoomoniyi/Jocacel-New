01e57f9afe39b00a392c70e4e2ff248671f5b5c7
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2024-06-28 09:58:08 +0100

    Add contact details

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,21 +28,29 @@ const Contact = () => {
 					</div>
 				</div>
 				<div className="flex flex-col gap-2">
-					<a href="" className="flex flex-row items-center gap-2">
+					<a
+						href="mailto:kadafpropertiesllp@gmail.com"
+						className="flex flex-row items-center gap-2"
+					>
 						<img
 							src={Email}
 							alt=""
 							className="inline p-2 rounded-full bg-primary"
 						/>
-						<div className="underline">JamesOthniel@gmail.com</div>
+						<div className="underline">
+							kadafpropertiesllp@gmail.com
+						</div>
 					</a>
-					<a href="" className="flex flex-row items-center gap-2">
+					<a
+						href="tel:08064546214"
+						className="flex flex-row items-center gap-2"
+					>
 						<img
 							src={Call}
 							alt=""
 							className="inline-flex items-center justify-center p-2 rounded-full h-11 w-11 bg-primary"
 						/>
-						<div className="underline">+234 808 368 7240</div>
+						<div className="underline">0806 454 6214</div>
 					</a>
 				</div>
 			</div>
